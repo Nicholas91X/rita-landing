586920c9a33d9a6412d8aeb6b586dc960e868529
Nicholas Romiti <romitinicholas91@hotmail.it>
2025-09-08 17:46:27 +0200

    Added padding on page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -34,7 +34,7 @@ export default function Page() {
         speedSec={22}
       />
 
-      <div className="max-w-6xl mx-auto px-8 md:px-8 lg:px-12">
+      <div className="max-w-6xl mx-auto px-8 md:px-8 lg:px-24 xl:px-18">
         <Hero />
         <Metodo />
         <PerChi />
